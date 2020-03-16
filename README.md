61071afec10ae545a6e0ab020efc37d7dbe19b91
Knightron1 <61396001+Knightron1@users.noreply.github.com>
2020-03-16 13:57:17 +1300

    switch cases

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SDV503-Week4
+What did we learn?
+
+
+Why did we learn it?
+
+
+How does it help?
+
--- index.js
+++ index.js
@@ -39,7 +39,48 @@ switch (person) {
     console.log('Hi MJ');
     break;
   default:
-    console.log('Hi');  //The Default is useful in case there is no case matching the input, as it will show a response even if there is no matching case
+    console.log('Hi');  //The Default is useful in case there is no value matching the input, as it will show a response even if there is no matching case
     break;
 }
 
+// const foo = -3;
+// switch (foo) {
+//   case 1:
+//   case 2:
+//   case 3:
+//   case 4:
+//     console.log('I like this number');
+//     break;
+//   case -1:
+//   case -2:
+//   case -3:
+//   case -4:
+//     console.log('I love this number');
+//     break;
+//   default:
+//      console.log('I don\'t like this number.');
+// }
+
+const foo = 1;
+let output = '';
+switch (foo) {
+  case 0:
+    output += 'So ';
+  case 1:
+    output += 'How ';
+    output += 'Old ';
+  case 2:
+    output += 'Are ';
+  case 3:
+    output += 'You';
+  case 4:
+    output += '?';
+    console.log(output);
+    break;
+  case 5:
+    output += 'Old?';
+    console.log(output);
+    break;
+  default:
+    console.log('Please pick a number from 0 to 5!');
+}   
