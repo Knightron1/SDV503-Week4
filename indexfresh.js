5c0387aeb7f1260482588d8a3eb436d2ac9e11e8
Knightron1 <61396001+Knightron1@users.noreply.github.com>
2020-04-06 14:18:09 +1200

    lab3 fin

--- indexfresh.js
+++ indexfresh.js
@@ -5,3 +5,23 @@ let x = 5
 
 let y = x > 4 ? 'true' : 'false';
 console.log(y)
+
+function foo(x, y, z, d, ...args) {
+    return console.log (x,y,z,d, args)
+}
+
+foo(1, 2, 3, 4, 5, 6, 7)
+
+function asd(...args){
+    console.log(args[5])
+}
+
+var arr = [1,2,3,4,5,6,7]
+
+asd(...arr)
+
+function top(...testing){
+    console.log(testing[4]);
+}
+
+top(1, 'two', true, 'three', 'four')
